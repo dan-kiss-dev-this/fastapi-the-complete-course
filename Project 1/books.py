e7f4ce96fda426ced663a3cd91bbac58417d0ba1
Daniel Kiss <flowfast47@gmail.com>
2026-07-13 18:03:37 -0700

    Add new endpoints

--- Project 1/books.py
+++ Project 1/books.py
@@ -15,38 +15,65 @@ BOOKS = [
 async def first_api():
     return {'message': 'Hello Dan!'}
 
-#return category by query
 @app.get("/books")
-async def read_category_by_query(category: str):
-    books_to_return = []
-    for book in BOOKS:
-        if book.get('category') == category:
-            books_to_return.append(book)
-    return books_to_return
-
-# @app.get("/books")
-# async def read_all_books():
-#     return BOOKS
-
-@app.get("/books/{book_author}")
-async def read_category_by_query(category: str):
-    books_to_return = []
-    for book in BOOKS:
-        if book.get('category').casefold() == category.casefold():
-            books_to_return.append(book)
-    return books_to_return
+async def read_all_books():
+    return BOOKS
 
 @app.get("/books/favorite")
 async def read_book():
     return { 'book_title': 'favorite book'}
 
+# return by path param
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
 
+#return category by query
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category') == category:
+            books_to_return.append(book)
 
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold() and \
+                book.get('author').casefold() == book_author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+@app.put("/books/update_book")
+async def update_book(update_book=Body()):
+    for i in range(len(BOOKS)):
+        if(BOOKS[i].get('title').casefold() == update_book.get('title').casefold()):
+            BOOKS[i] = update_book
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+
+#get all books from a specific author
+@app.get("/books/byauthor/{author}")
+async def get_books_by_author(author: str):
+    books_by_author = []
+    for book in BOOKS:
+        if book.get("author").casefold() == author.casefold():
+            books_by_author.append(book)
+    return books_by_author
 
 # from fastapi import Body, FastAPI
 #
@@ -63,16 +90,7 @@ async def read_book(book_title: str):
 # ]
 #
 #
-# @app.get("/books")
-# async def read_all_books():
-#     return BOOKS
-#
-#
-# @app.get("/books/{book_title}")
-# async def read_book(book_title: str):
-#     for book in BOOKS:
-#         if book.get('title').casefold() == book_title.casefold():
-#             return book
+
 #
 #
 # @app.get("/books/")
@@ -124,3 +142,9 @@ async def read_book(book_title: str):
 #         if BOOKS[i].get('title').casefold() == book_title.casefold():
 #             BOOKS.pop(i)
 #             break
+
+
+#to run debugger
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("books:app", host="0.0.0.0", port=8000, reload=True)
