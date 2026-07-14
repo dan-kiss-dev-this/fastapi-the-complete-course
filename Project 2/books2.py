1dd41fc2e1d88089ad11267f3ec4fbb8d90be17c
Daniel Kiss <flowfast47@gmail.com>
2026-07-14 14:27:26 -0700

    create BookRequest with pydantic data validation including incrementing id and default value

--- Project 2/books2.py
+++ Project 2/books2.py
@@ -1,5 +1,84 @@
 from typing import Optional
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
 
+app = FastAPI()
+
+
+class Book():
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int, year: int) -> None:
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+        self.year = year
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(description="Id is not needed on create", default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=6)
+    year: int
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "codingwithroby",
+                "description": "A new description of a book",
+                "rating": 5,
+                "year": 2026
+            }
+        }
+    }
+
+
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2030),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5, 2030),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5, 2029),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2, 2028),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2027),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1, 2026)
+]
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    print(type(new_book))
+
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    # if len(BOOKS) > 0:
+    #     book.id = BOOKS[-1].id + 1
+    # else:
+    #     book.id = 1
+
+    return book
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("books2:app", host="0.0.0.0", port=8000, reload=True)
 
 # from typing import Optional
 # from fastapi import FastAPI, Path, Query, HTTPException, Body
