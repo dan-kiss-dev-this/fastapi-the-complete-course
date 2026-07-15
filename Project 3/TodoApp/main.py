360eaf842891e080e3ee8b1c7f58d1e0beac1d18
Daniel Kiss <flowfast47@gmail.com>
2026-07-15 15:35:20 -0700

    add dependency injection and first endpoint to show database

--- Project 3/TodoApp/main.py
+++ Project 3/TodoApp/main.py
@@ -1,3 +1,40 @@
+from typing import Annotated
+from sqlalchemy.orm import Session
+
+# root folder for fastapi
+from fastapi import FastAPI, Depends
+import models
+from models import Todos
+from database import engine, SessionLocal
+
+
+
+app = FastAPI()
+
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    # so connect to db and close db connection after response delivered, open db connection only when using database and close after
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+# note Depends below is dependency injection
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+#
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
 
 
 # from fastapi import FastAPI
