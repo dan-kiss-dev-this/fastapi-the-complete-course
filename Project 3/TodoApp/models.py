da648ca26a42bb50b8c1dde4f0064d10374dc469
Daniel Kiss <flowfast47@gmail.com>
2026-07-15 15:34:31 -0700

    add a model

--- Project 3/TodoApp/models.py
+++ Project 3/TodoApp/models.py
@@ -1,3 +1,17 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+
+class Todos(Base):
+    __tablename__ = "todos"
+
+    # means indexable
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+
 
 
 # from database import Base
