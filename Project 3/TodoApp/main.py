e6ad431f8241d23be05ebff75ddcd635ea725708
Daniel Kiss <flowfast47@gmail.com>
2026-07-17 15:11:28 -0700

    comment code

--- Project 3/TodoApp/main.py
+++ Project 3/TodoApp/main.py
@@ -12,10 +12,10 @@ app.include_router(auth.router)
 app.include_router(todos.router)
 
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 
