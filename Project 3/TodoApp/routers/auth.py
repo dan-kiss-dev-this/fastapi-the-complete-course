4380f6728704944d958d26f3b9d1df95ae52fba9
Daniel Kiss <flowfast47@gmail.com>
2026-07-17 15:11:42 -0700

    add password hashing

--- Project 3/TodoApp/routers/auth.py
+++ Project 3/TodoApp/routers/auth.py
@@ -1,7 +1,34 @@
 from fastapi import FastAPI, APIRouter
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
+
 
 router = APIRouter()
 
-@router.get("/auth/")
-async def get_user():
-    return {"user": "authenticated"}
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+#pydantics class does field validation, leave id and is_active out
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+# note below need to write out for the hashed password setup
+@router.post("/auth")
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+
+        is_active=True
+    )
+
+    return create_user_model
