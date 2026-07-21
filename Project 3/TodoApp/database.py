c8263d91aaf2b2ec94120b322ca58175e86a7f53
Daniel Kiss <flowfast47@gmail.com>
2026-07-21 16:26:18 -0700

    add alembic and db migration

--- Project 3/TodoApp/database.py
+++ Project 3/TodoApp/database.py
@@ -3,10 +3,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234@localhost/TodoApplicationDatabase"
+
+# old sqlite implementation
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234@localhost/TodoApplicationDatabase"
 
 # allow multiple threads to interact with database
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# old engine sqlite3
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
